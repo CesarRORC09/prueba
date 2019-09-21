9b5d349a2bb31490af2b4229a998fb3e4a807613
Cesar Roman Reyes <src_graff@hotmail.com>
2019-09-20 23:48:04 -0500

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { SidenavComponent } from 'src/components/sidenav/sidenav.component';
 
 
 const routes: Routes = [
- // { path:'',redirectTo: 'login', pathMatch: 'full'},
+ //{ path:'',redirectTo: 'login', pathMatch: 'full'},
  { path: 'login',  component:LoginComponent},
  { path: 'tablero' , component:HomeComponent},
 
